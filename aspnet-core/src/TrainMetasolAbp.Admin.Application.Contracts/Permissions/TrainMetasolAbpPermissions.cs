be27c0c4538cdcd6b2e6a180a2892f08079c4e1f
ndtThinh <thinhndt1001@gmail.com>
2024-06-22 10:23:40 +0700

    finsh stucture

---
+++ aspnet-core/src/TrainMetasolAbp.Admin.Application.Contracts/Permissions/TrainMetasolAbpPermissions.cs
@@ -0,0 +1,9 @@
+﻿namespace TrainMetasolAbp.Admin.Permissions;
+
+public static class TrainMetasolAbpPermissions
+{
+    public const string GroupName = "TrainMetasolAbp";
+
+    //Add your own permission names. Example:
+    //public const string MyPermission1 = GroupName + ".MyPermission1";
+}
---
+++ aspnet-core/src/TrainMetasolAbp.Admin.Application/TrainMetasolAbpAdminAppService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TrainMetasolAbp.Localization;
+using Volo.Abp.Application.Services;
+
+namespace TrainMetasolAbp.Admin;
+
+/* Inherit your application services from this class.
+ */
+public abstract class TrainMetasolAbpAdminAppService : ApplicationService
+{
+    protected TrainMetasolAbpAdminAppService()
+    {
+        LocalizationResource = typeof(TrainMetasolAbpResource);
+    }
+}
